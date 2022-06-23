b2233402532f34af1749b6b2c39f261d834079dd
BenHur Albertassi <benhuralber@yahoo.com.br>
2022-06-23 16:00:51 -0300

    aula de hoje

---
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/script.js
@@ -0,0 +1,51 @@
+let fatorial = 1;
+for (let index = 10; index > 0; index--) {
+    fatorial *= index;
+    
+}
+console.log(fatorial);
+
+let word = 'tryber';
+let rev ='';
+
+for (let index = 0; index < word.length; index++) {
+    rev += word[word.length - 1 - index];
+    
+}
+
+console.log (rev)
+
+
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+
+let maior = array[0]
+let menor = array[0]
+
+for (let index = 0; index < array.length; index++) {
+    if (array[index] > maior.length) {
+        maior = array [index];
+    }
+}
+
+for (let index = 0; index < array.length; index += 1) {
+    if (array[index] < menor.length) {
+        menor = array [index];
+    }
+}
+console.log(maior);
+console.log(menor);
+
+
+let n = 5;
+let c = '*'
+let total = '';
+
+for(let index = 0; index < n; index++ ){
+    total = total + c;
+};
+
+for (let index = 0; index < n; index++) {
+console.log(total);
+}
+
+    
