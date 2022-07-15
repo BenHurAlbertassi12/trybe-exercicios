d3a2c203b5cddcf9c73bb6d7f2ec0717615aa794
BenHur Albertassi <benhuralber@yahoo.com.br>
2022-07-15 19:01:22 -0300

    Aulas

---
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-fluxo-de-excecao-e-objetos/aula72.js
@@ -0,0 +1,44 @@
+const verifyIsNumber = (value1, value2) => {
+    if (typeof value1 !== 'number' || typeof value2 !== 'number') {
+      throw new Error('Os valores devem ser numéricos');
+    }
+  };
+  
+  const sum = (value1, value2) => {
+    try {
+      verifyIsNumber(value1, value2);
+      return value1 + value2;
+    } catch (error) {
+      return error.message;
+    }
+  };
+  
+  console.log(sum(2, '3'));
+
+
+  const customer1 = {
+    firstName: 'Roberto',
+    age: 22,
+    job: 'Teacher',
+  };
+  
+  console.log(customer1);
+  
+  customer1.lastName = 'Faria';
+  console.log(customer1);
+  
+
+  
+  const customer = {
+    firstName: 'Roberto',
+    age: 22,
+    job: 'Teacher',
+  };
+  
+  let newKey = 'lastName';
+  const lastName = 'Ferreira';
+  customer[newKey] = lastName;
+  newKey = 'fullName';
+  const fullName = `${customer.firstName} ${customer.lastName}`;
+  customer[newKey] = fullName;
+  console.log(customer);
