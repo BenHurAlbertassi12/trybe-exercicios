ba4a01408711673ae33773b190e3bdd364e3e436
BenHur Albertassi <benhuralber@yahoo.com.br>
2022-07-18 15:57:43 -0300

    Conteudo do dia 7.3

--- bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/conteudo/script.test.js
+++ bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/conteudo/script.test.js
@@ -1,6 +1,7 @@
 const {expect} = require ('expect');
-const sum = require('./script');
+const sum = require('.script.js');
 
 test ('Verifica se a soma de 1 e 2 é 3', () => {
     expect(sum(1, 2)).toBe(3);
 })
+
