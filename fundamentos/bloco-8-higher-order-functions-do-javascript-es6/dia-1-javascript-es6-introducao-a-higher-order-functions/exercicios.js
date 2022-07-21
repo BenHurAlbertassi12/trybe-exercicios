c04e8101c344b8cc3a9a854e03fa1acc98a90b4b
BenHur Albertassi <benhuralber@yahoo.com.br>
2022-07-21 18:37:41 -0300

    exercicio 2

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/exercicios.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/exercicios.js
@@ -5,7 +5,7 @@ const employeeGenerator = (fullName) => {
   
   const newEmployees = (callback) => {
     const employees = {
-      id1: callback('Pedro Guerra'),
+      id1: callback('BenHur Albertassi'),
       id2: callback('Luiza Drumond'),
       id3: callback('Carla Paiva'),
     };
@@ -13,3 +13,12 @@ const employeeGenerator = (fullName) => {
   };
   
   console.log(newEmployees(employeeGenerator));
+
+  // -------------------------------------------------------------------
+
+  const numeroChck = (meuNumero, number) => meuNumero === number ;
+    const resultadoSorteio = (meuNumero, callback) =>{
+        const number = Math.floor((Math.random()*5)+1)
+        return callback (meuNumero, number) ? 'Parabens você ganhou' : 'Tente novamente'
+  }
+  console.log(resultadoSorteio(2, numeroChck));
