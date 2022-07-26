c2a9ebdae0b13a49913d4c57d778d9da6071820b
BenHur Albertassi <benhuralber@yahoo.com.br>
2022-07-26 15:16:22 -0300

    test new font

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exercicio1.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exercicio1.js
@@ -90,7 +90,7 @@ const books = [
 
 
 function everyoneWasBornOnSecXX() {
-  return books.every((book) => (
+  return books.every((book) => ( 
     book.author.birthYear > 1900 && book.author.birthYear <= 2000
   ));
 }
