81e21a64801102c5ab4a505bad2c5351943cf7dc
BenHur Albertassi <benhuralber@yahoo.com.br>
2022-08-04 18:22:41 -0300

    encerramento conteudo

--- front-end/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/conteudo.js
+++ front-end/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/conteudo.js
@@ -1,31 +1,18 @@
-const countryName = ({ name }) => console.log(`Returned country is ${name}`);
-const countryCurrency = ({ name, currency }) => console.log(`${name}'s currency is the ${currency}`);
+let timeA = 1000;
+let timeB = 500;
+let numA = 4;
+let numB = 6;
+let numC = 2;
 
-const delay = (maxMilliseconds = 5000) => Math.floor(Math.random() * maxMilliseconds);
+const sum = (a, b) => a + b;
 
-const printErrorMessage = (error) => console.log(`Error getting country: ${error}`);
+setTimeout(() => {
+    numC = sum(numA, numB);
+    console.log(numC);
+}, timeA);
 
-const getCountry = (onSuccess, callback) => {
-    setTimeout(() => {
-        const didOperationSucceed = Math.random() >= 0.5;
-        if (didOperationSucceed) {
-            const country = {
-                name: "Brazil",
-                hdi: 0.759,
-                currency: "Real",
-            };
-            onSuccess(country);
-        } else {
-            const errorMessage = "Country could not be found";
-            return callback(errorMessage)
-        }
+console.log(numC);
 
-    }, delay());
-    // return callback(getCountry)
-};
-
-// Deve imprimir "Returned country is Brazil" no sucesso ou "Error getting country: Country could not be found" em caso de falha
-getCountry(countryName, printErrorMessage);
-
-// Deve imprimir "Brazil's currency is the Real" no sucesso, ou "Error getting country: Country could not be found" em caso de falha
-getCountry(countryCurrency, printErrorMessage);
+setTimeout(() => {
+    console.log(sum(numA, numC) === numB);
+}, timeB);
