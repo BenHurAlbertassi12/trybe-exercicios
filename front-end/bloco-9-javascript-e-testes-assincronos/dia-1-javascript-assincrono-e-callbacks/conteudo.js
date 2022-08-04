1e90082e498c9e5945870878e4a3f20c842bfdae
BenHur Albertassi <benhuralber@yahoo.com.br>
2022-08-04 12:52:07 -0300

    conteudo callback 1

--- front-end/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/conteudo.js
+++ front-end/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/conteudo.js
@@ -1,33 +1,15 @@
-const despesas = [
-    {
-        gym: 99,
-    },
-    {
-        ifood: 200,
-    },
-    {
-        phone: 60,
-    },
-    {
-        internet: 100,
-    },
-];
+const userFullName = ({ firstName, lastName }) => `Hello! My name is ${firstName} ${lastName}`;
+const userNationality = ({ firstName, nationality }) => `${firstName} is ${nationality}`;
 
-const renda = 1000;
+const getUser = (callback) => {
+    const userToReturn = {
+        firstName: "Ivan",
+        lastName: "Ivanovich",
+        nationality: "Russian"
+    };
+    // Insira o retorno da função `getUser`
+    return callback(userToReturn);
+};
 
-const despesaMensal = (renda, despesas, callback) => {
-    const despesaTotal = callback(despesas);
-    const saldoFinal = renda - despesaTotal
-
-    console.log(`Balanço do mês: 
-    Recebido: R$${renda},00 
-    Gasto: ${despesaTotal},00 
-    Saldo: ${saldoFinal},00`);
-}
-
-const somaDespesas = (despesas) => {
-    const custoItem = despesas.map((item) => Object.values(item));
-    const despesaTotal = custoItem.reduce((acc, curr) => acc += curr[0], 0);
-    return despesaTotal;
-}
-despesaMensal(renda, despesas, somaDespesas)
+console.log(getUser(userFullName)); // complete a chamada da função getUser de modo que o retorno seja: "Hello! My name is Ivan Ivanovich"
+console.log(getUser(userNationality)); // complete a chamada da função getUser de modo que o retorno seja: "Ivan is Russian"
