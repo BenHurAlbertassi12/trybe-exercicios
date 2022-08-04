fa01e40a9f37707e3c7e2b8c1bb6ed05d07c29c4
BenHur Albertassi <benhuralber@yahoo.com.br>
2022-08-04 10:52:21 -0300

    upando ultimos exercicios

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/MapFilter/template.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/MapFilter/template.js
@@ -62,3 +62,37 @@ const books = [
 ];
 
 // Adicione o código do exercício aqui:
+
+function formatedBookNames() {
+  return books.map((book)=> `${book.name} - ${book.genre} - ${book.author.name}`);
+  // escreva seu código aqui
+ 
+}
+console.log('exercicio1');
+console.log(formatedBookNames());
+
+//---------------------------------------------------------------------------
+
+function nameAndAge() { 
+  return books.map((book) => ({
+    author: book.author.name,
+    age: book.releaseYear - book.author.birthYear
+  })).sort((obj1, obj2) => obj1.age - obj2.age)
+
+  // escreva seu código aqui
+}
+console.log('exercicio2');
+console.log(nameAndAge());
+
+//---------------------------------------------------------------------------
+function fantasyOrScienceFiction() {
+  // escreva seu código aqui
+  return books.filter((book) => (book.genre === 'Fantasia' || book.genre === 'Ficção Científica'));
+}
+console.log('exercicio3');
+console.log(fantasyOrScienceFiction());
+
+//---------------------------------------------------------------------------
+
+
+
