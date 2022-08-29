21914d3f963e37a8c17592248d4027bf5a5c24f0
BenHur Albertassi <benhuralber@yahoo.com.br>
2022-08-29 14:55:38 -0300

    Link

--- frontend/aula12.2/src/components/About.jsx
+++ frontend/aula12.2/src/components/About.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class About extends React.Component{
     render(){
-        return <span> Se meu navio de React é o de teseu e eu refatorei todos os seus componentes, ele ainda é o meu navio? </span>
-    }
+        return (
+        <div>
+            <Link to="/">Voltar á home</Link>
+            <span> Se meu navio de React é o de teseu e eu refatorei todos os seus componentes, ele ainda é o meu navio? </span>
+        </div>
+    )}
 }
 
 export default About;
--- frontend/aula12.2/src/components/Home.jsx
+++ frontend/aula12.2/src/components/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Home extends React.Component{
     render(){
         return (
         <div>
+            <Link to="/about">About</Link>
             <h1>Minha Home Page</h1>
         </div>
     )}
